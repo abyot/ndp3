ffc9618367ed51f3070c725358e87a7ce4e044e7
Abyot Asalefew Gizaw <abyota@gmail.com>
2020-11-06 13:34:47 +0100

    Project time and cost performance

--- dhis-2/dhis-web/dhis-web-apps/src/main/webapp/dhis-web-ndp-framework/components/project/project-controller.js
+++ dhis-2/dhis-web/dhis-web-apps/src/main/webapp/dhis-web-ndp-framework/components/project/project-controller.js
@@ -1,6 +1,6 @@
 /* Controllers */
 
-/* global ndpFramework */
+/* global ndpFramework, parseFloat */
 
 ndpFramework.controller('ProjectController',
     function($scope,
@@ -36,7 +36,11 @@ ndpFramework.controller('ProjectController',
         periodOffset: 0,
         openFuturePeriods: 10,
         selectedPeriodType: 'FinancialJuly',
-        coreProjectAttribute: null
+        coreProjectAttribute: null,
+        bac: null,
+        ac: null,
+        timePerformance: [],
+        costPerformance: []
     };
 
     $scope.model.horizontalMenus = [
@@ -183,6 +187,9 @@ ndpFramework.controller('ProjectController',
                 if( pta.trackedEntityAttribute.isCoreProject ){
                     $scope.model.coreProjectAttribute = pta.trackedEntityAttribute;
                 }
+                else if( pta.trackedEntityAttribute.isTotalProjectCost ){
+                    $scope.model.bac = pta.trackedEntityAttribute;
+                }
             });
 
             angular.forEach($scope.model.selectedProgram.programStages, function(stage){
@@ -190,6 +197,10 @@ ndpFramework.controller('ProjectController',
                     var de = prstDe.dataElement;
                     if( de ){
                         $scope.model.dataElementsById[de.id] = de;
+
+                        if( de.isCurrentProjectCost ){
+                            $scope.model.ac = de;
+                        }
                     }
                 });
             });
@@ -208,9 +219,85 @@ ndpFramework.controller('ProjectController',
         }
         else{
             $scope.model.showProjectDetails = true;
+            $scope.model.timePerformance = [];
+            $scope.model.costPerformance = [];
             if( project && project.trackedEntityInstance && $scope.model.selectedProgram ){
                 ProjectService.get( project, $scope.model.selectedProgram, $scope.model.optionSetsById, $scope.model.attributesById , $scope.model.dataElementsById ).then(function( data ){
+
                     $scope.model.selectedProject = data;
+
+                    //Get BAC
+                    var atvs = $scope.model.selectedProject.attributes;
+                    var bacv = '';
+                    if ( atvs && atvs.length > 0 ){
+                        angular.forEach(atvs, function(atv){
+                            if( atv.attribute === $scope.model.bac.id ){
+                                bacv = atv.value;
+                            }
+                        });
+                    }
+
+                    var tpBsv = 56.4, cpBsv = 84.6;
+                    var tpEvWeight = 2, tpPvWeight = 8, cpEvWeight = 1;
+
+                    //Get AC
+                    var en = $scope.model.selectedProject.enrollments[0];
+                    var tpBac = {KPI: 'BAC', IND: $scope.model.bac.displayName, INT: $translate.instant('original_project_budget'), UNI: 'Bn UGX', BSL: bacv, qvs: []};
+                    var tpAc = {KPI: 'AC', IND: $translate.instant('ac'), INT: $scope.model.ac.displayName, UNI: 'Bn UGX', BSL: tpBsv, qvs: []};
+                    var tpEv = {KPI: 'EV', IND: $translate.instant('ev'), INT: $translate.instant('approved_budget_for_performed_project_activities'), UNI: 'Bn UGX', BSL: parseFloat( (tpEvWeight / 10) * bacv ).toFixed(2), qvs: []};
+                    var tpPv = {KPI: 'PV', IND: $translate.instant('pv'), INT: $translate.instant('estimated_cost_of_planned_project_activities'), UNI: 'Bn UGX', BSL: parseFloat( (tpPvWeight / 10) * bacv ).toFixed(2), qvs: []};
+                    var tpSpi = {KPI: 'SPI', IND: $translate.instant('spi'), INT: $translate.instant('project_progress_vs_schedule'), UNI: '0.01 - 1.00', BSL: parseFloat(tpEv.BSL / tpPv.BSL).toFixed(2), qvs: []};
+                    var tpSv = {KPI: 'SV',  IND: $translate.instant('sv'),  INT: $translate.instant('sv_shows_if_project_is_ahead_or_behind_schedule'),  UNI: 'Bn UGX (+ve or -ve)', BSL: tpEv.BSL - tpPv.BSL, qvs: []};
+
+
+                    var cpBac = {KPI: 'BAC', IND: $scope.model.bac.displayName, INT: $translate.instant('original_project_budget'), UNI: 'Bn UGX', BSL: bacv, qvs: []};
+                    var cpAc = {KPI: 'AC', IND: $translate.instant('ac'), INT: $scope.model.ac.displayName, UNI: 'Bn UGX', BSL: cpBsv, qvs: []};
+                    var cpEv = {KPI: 'EV', IND: $translate.instant('ev'), INT: $translate.instant('approved_budget_for_performed_project_activities'), UNI: 'Bn UGX', BSL: parseFloat( (cpEvWeight / 10) * bacv ).toFixed(2), qvs: []};
+                    var cpCpi = {KPI: 'CPI', IND: $translate.instant('cpi'), INT: $translate.instant('value_of_work_completed_vs_actual_expenditure'), UNI: '0.01 - 1.00', BSL: parseFloat( cpEv.BSL / cpAc.BSL ).toFixed(2), qvs: []};
+                    var cpCv = {KPI: 'CV', IND: $translate.instant('cv'), INT: $translate.instant('cv_shows_whether_the_project_is_over_or_under_budget'), UNI: 'Bn UGX (+ve or -ve)', BSL: parseFloat( cpEv.BSL - cpAc.BSL ).toFixed(2), qvs: []};
+                    var cpEac = {KPI: 'EAC', IND: $translate.instant('eac'), INT: $translate.instant('expected_total_cost_of_completing_all_work'), UNI: 'Bn UGX', BSL: parseFloat(cpBsv / (cpEvWeight / 10)).toFixed(2), qvs: []};
+                    var cpEtc = {KPI: 'ETC',  IND: $translate.instant('etc'),  INT: $translate.instant('expected_cost_required_to_complete_the_remaining_project_work'),  UNI: 'Bn UGX', BSL: parseFloat(cpEac.BSL - cpAc.BSL).toFixed(2), qvs: []};
+
+                    if( en && en.events && en.events.length > 0){
+                        var index = 1;
+                        angular.forEach(en.events, function(ev){
+                            angular.forEach(ev.dataValues, function(dv){
+                                if( dv.dataElement === $scope.model.ac.id ){
+
+                                    //Calcuate time performance values
+                                    var tpEvVal = parseFloat( ((tpEvWeight + index) / 10) * bacv ).toFixed(2);
+                                    var tpPvVal = parseFloat( ((tpPvWeight - index) / 10) * bacv ).toFixed(2);
+
+                                    tpBac.qvs.push( bacv );
+                                    tpAc.qvs.push( dv.value );
+                                    tpEv.qvs.push( parseFloat(tpEvVal).toFixed(2) );
+                                    tpPv.qvs.push( parseFloat(tpPvVal).toFixed(2) );
+                                    tpSpi.qvs.push( parseFloat(tpEvVal / tpPvVal).toFixed(2) );
+                                    tpSv.qvs.push( parseFloat(tpEvVal - tpPvVal).toFixed(2) );
+
+                                    //Calculate cost performance values
+                                    var cpEvVal = parseFloat( ((cpEvWeight + index) / 10) * bacv ).toFixed(2);
+                                    var cpCpiVal = parseFloat( cpEvVal / dv.value ).toFixed(2);
+                                    var cpCvVal = parseFloat( cpEvVal - dv.value ).toFixed(2);
+                                    //var cpEacVal = parseFloat( bacv / cpCpiVal ).toFixed(2);
+                                    var cpEacVal = parseFloat(dv.value / ((cpEvWeight + index)/ 10)).toFixed(2);
+                                    var cpEtcVal = parseFloat( cpEacVal - dv.value ).toFixed(2);
+
+                                    cpBac.qvs.push( bacv );
+                                    cpAc.qvs.push( dv.value );
+                                    cpEv.qvs.push( cpEvVal );
+                                    cpCpi.qvs.push( cpCpiVal );
+                                    cpCv.qvs.push( cpCvVal );
+                                    cpEac.qvs.push( cpEacVal );
+                                    cpEtc.qvs.push( cpEtcVal );
+                                }
+                            });
+                            index++;
+                        });
+
+                        $scope.model.timePerformance = [tpBac, tpAc, tpEv, tpPv, tpSpi, tpSv];
+                        $scope.model.costPerformance = [cpBac, cpAc, cpEv, cpCpi, cpCv, cpEac, cpEtc];
+                    }
                 });
             }
         }
@@ -225,10 +312,7 @@ ndpFramework.controller('ProjectController',
 
     $scope.resetView = function(horizontalMenu, e){
         $scope.model.activeHorizontalMenu = horizontalMenu;
-
-
         if(e){
-            console.log('preventing default ...');
             e.stopPropagation();
             e.preventDefault();
         }
@@ -255,7 +339,6 @@ ndpFramework.controller('ProjectController',
         modalInstance.result.then(function ( selectedOu ) {
             if( selectedOu && selectedOu.id ){
                 $scope.selectedOrgUnit = selectedOu;
-                $scope.resetDataView();
             }
         });
     };
